5c8dc7c745d8bf68bc4c1ad41a458d231566bcc7
Mamun1646 <mimbd0113000gmail.com>
2022-10-12 17:30:10 +0600

    Lecture2.js

---
+++ Lecture2.js
@@ -0,0 +1,26 @@
+//Javasript Operator Precedence
+//Concept of Data Types
+let length = 16;                               // Number
+let lastName = "Johnson";                      // String
+let x = { firstName: "John", lastName: "Doe" };    // Object
+// JavaScript evaluates expressions from left to right. Different sequences can produce different results:
+let result = "Volvo" + 16 + 4; //Volvo164
+let result2 = 16 + 4 + "Volvo"; //20Volvo
+// JavaScript Types are Dynamic
+// JavaScript has dynamic types. This means that the same variable can be used to hold different data types:
+let data;           // Now x is undefined
+data= 5;           // Now x is a Number
+data = "John";      // Now x is a String
+
+
+
+//String define 
+let carName1 = "Volvo XC60";   // Using double quotes
+let carName2 = 'Volvo XC60';   // Using single quotes
+
+//javasript number
+let x1 = 34.00;     // Written with decimals
+let x2 = 34;        // Written without decimals
+
+
+//Learn more from w3schools
