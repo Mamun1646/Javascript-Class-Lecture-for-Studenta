ac71f847d2baf865b39e3c5d6f9a1bd5f066e67f
Mamun1646 <mimbd0113000gmail.com>
2022-10-12 17:14:45 +0600

    Lecture1.js

---
+++ Lecture1.js
@@ -0,0 +1,53 @@
+// JavaScript Display Possibilities
+// Writing into an HTML element, using innerHTML.
+// Writing into the HTML output using document.write().
+// Writing into an alert box, using window.alert().
+// Writing into the browser console, using console.log().
+
+
+console.log("Hello Javasript console");
+document.write("Hello Javasript Document")
+window.alert("hello window");
+alert("You can Skip Window Keyword");
+document.getElementById("demo").innerHTML = "Hello Inner Html";
+
+//What is statement
+//Answer : Eveny Line Is a single statement.Javasript Compiler Multiple White Space Ignored
+
+
+
+// Keyword	Description
+// var->	Declares a variable
+// let ->	Declares a block variable
+// const->	Declares a block constant
+// if	->Marks a block of statements to be executed on a condition
+// switch ->	Marks a block of statements to be executed in different cases
+// for ->	Marks a block of statements to be executed in a loop
+// function	->Declares a function
+// return	->Exits a function
+// try	->Implements error handling to a block of statements
+
+
+
+//Javasript Syntax
+//var let const varible and function name declaretion
+//they are also variable
+var x = 3;
+let y = "Mamun";
+const z = "Al Mamun";
+let sum = 3 + 4;
+
+
+// Comment :Just Write Double Slash Before
+
+
+
+//Operator
+//Easy to learn from javasript
+
+
+
+
+
+
+
